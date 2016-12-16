faa6fdc22c864c6499d1ebd31d80238d74ed109e
Ugo Cupcic <ugo@shadowrobot.com>
2016-12-16 12:09:21 +0000

    Working in gzweb

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ export DISPLAY
 source "/workspace/devel/setup.bash"
 source "/usr/share/gazebo/setup.sh"
 
-GAZEBO_MODEL_PATH=/workspace/src/universal_robot:${GAZEBO_MODEL_PATH} roslaunch ur_gazebo ur10.launch gui:=false gzweb:=true verbose:=true&
+GAZEBO_MODEL_PATH=/workspace/src:${GAZEBO_MODEL_PATH} roslaunch ur_gazebo ur10.launch gui:=false gzweb:=true verbose:=true&
 
 sleep 5
 
 cd /root/gzweb
-GAZEBO_MODEL_PATH=/workspace/src/universal_robot:${GAZEBO_MODEL_PATH} ./start_gzweb.sh
+GAZEBO_MODEL_PATH=/workspace/src:${GAZEBO_MODEL_PATH} ./start_gzweb.sh
 
 read -n 1 -s
--- setup_gzweb.sh
+++ setup_gzweb.sh
@@ -4,7 +4,7 @@ DISPLAY=:1.0
 export DISPLAY
 
 cd /root
-hg clone -r gzweb_2.0.0 https://bitbucket.org/osrf/gzweb
+hg clone -r gzweb_1.3.0 https://bitbucket.org/osrf/gzweb
 source "/workspace/devel/setup.bash"
 source "/usr/share/gazebo/setup.sh"
 cd /root
@@ -14,4 +14,4 @@ for i in * ; do tar -zvxf "$i/model.tar.gz" ; done
 mkdir -p $HOME/.gazebo/models
 cp -vfR * "$HOME/.gazebo/models/"
 cd /root/gzweb
-GAZEBO_MODEL_PATH=/workspace/src/universal_robot:${GAZEBO_MODEL_PATH} ./deploy.sh -m
+GAZEBO_MODEL_PATH=/workspace/src:${GAZEBO_MODEL_PATH} ./deploy.sh -m
